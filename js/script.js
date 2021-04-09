440d382d5bfa9747d72ca75a108555f03a7036f6
example@gmail.com <iphahaha@gmail.com>
2021-04-09 11:16:14 +0300

    commit

---
+++ js/script.js
@@ -0,0 +1,80 @@
+"use strict";
+
+//let number = 4; 
+
+//console.log(4/0);
+//console.log('string' * 0);
+
+//const person = "alex";
+
+//const bool = true;
+
+
+//let und;
+//console.log(und);
+  
+
+/*const obj = {
+  name: 'Yuri',
+  age: 25,
+  isMarried: false
+};
+
+console.log(obj.name);
+
+let arr=['plum.png','orange.jpeg','apple.bmp'
+
+];
+
+console.log(arr[1]);*/
+
+
+//const result=confirm('are you here?');
+//console.log(result);
+
+//const answer=+prompt("are you 18?", "18");
+//console.log(answer+5);
+
+// const answers = [];
+
+//answers[0] = prompt("what is your name","");
+//answers[1] = prompt("what is your secondname", "");
+//answers[2] = prompt("how old are you", "");
+
+//console.log(typeof(answers));
+
+//ІНТЕРПОЛЯЦІЯ
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user="ivan";
+alert(`privet, ${user}`);
+
+//ОПЕРАТОРИ
+
+console.log(4 + "5");
+
+let incr = 10,
+    decr = 10;
+
+//ПОСТФІКС ПРЕФІКС
+
+// ++incr;
+// --decr
+
+console.log(incr);
+console.log(decr);
+
+
+console.log(5%2);
+console.log(2 + 2 * 2 === '8');
+
+// OPERATORU && ||
+
+const isCheked = true,
+      isClosed = false;
+
+console.log(isCheked && !isClosed);
+      
